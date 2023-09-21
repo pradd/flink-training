3948e77eb22f290711d8735841e07d5c93542dae
Dzmitry Zaitsau <Dzmitry.Zaitsau@ig.com>
2023-09-21 19:55:13 +0200

    Filtering a Stream (Ride Cleansing)

--- ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
+++ ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
@@ -26,6 +26,7 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
+import org.apache.flink.training.exercises.common.utils.GeoUtils;
 import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 
 /**
@@ -39,7 +40,9 @@ public class RideCleansingExercise {
     private final SourceFunction<TaxiRide> source;
     private final SinkFunction<TaxiRide> sink;
 
-    /** Creates a job using the source and sink provided. */
+    /**
+     * Creates a job using the source and sink provided.
+     */
     public RideCleansingExercise(SourceFunction<TaxiRide> source, SinkFunction<TaxiRide> sink) {
 
         this.source = source;
@@ -52,8 +55,7 @@ public class RideCleansingExercise {
      * @throws Exception which occurs during job execution.
      */
     public static void main(String[] args) throws Exception {
-        RideCleansingExercise job =
-                new RideCleansingExercise(new TaxiRideGenerator(), new PrintSinkFunction<>());
+        RideCleansingExercise job = new RideCleansingExercise(new TaxiRideGenerator(), new PrintSinkFunction<>());
 
         job.execute();
     }
@@ -76,11 +78,14 @@ public class RideCleansingExercise {
         return env.execute("Taxi Ride Cleansing");
     }
 
-    /** Keep only those rides and both start and end in NYC. */
+    /**
+     * Keep only those rides and both start and end in NYC.
+     */
     public static class NYCFilter implements FilterFunction<TaxiRide> {
         @Override
         public boolean filter(TaxiRide taxiRide) throws Exception {
-            throw new MissingSolutionException();
+            return GeoUtils.isInNYC(taxiRide.startLon, taxiRide.startLat) &&
+                    GeoUtils.isInNYC(taxiRide.endLon, taxiRide.endLat);
         }
     }
 }
