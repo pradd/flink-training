daa802c7f3196143e7583aea3e1bc42f3d525a99
Dzmitry Zaitsau <Dzmitry.Zaitsau@ig.com>
2023-11-28 12:39:55 +0100

    Windowed Analytics (Hourly Tips)

--- hourly-tips/src/main/java/org/apache/flink/training/exercises/hourlytips/HourlyTipsExercise.java
+++ hourly-tips/src/main/java/org/apache/flink/training/exercises/hourlytips/HourlyTipsExercise.java
@@ -19,75 +19,167 @@
 package org.apache.flink.training.exercises.hourlytips;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.training.exercises.common.datatypes.TaxiFare;
 import org.apache.flink.training.exercises.common.sources.TaxiFareGenerator;
-import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 /**
  * The Hourly Tips exercise from the Flink training.
  *
- * <p>The task of the exercise is to first calculate the total tips collected by each driver, hour
- * by hour, and then from that stream, find the highest tip total in each hour.
+ * <p>The task of the exercise is to first
+ * calculate the total tips collected by each driver,
+ * hour by hour, and then
+ * from that stream find the highest tip total in each hour.
  */
 public class HourlyTipsExercise {
 
-    private final SourceFunction<TaxiFare> source;
-    private final SinkFunction<Tuple3<Long, Long, Float>> sink;
+  private final SourceFunction<TaxiFare> source;
+  private final SinkFunction<Tuple3<Long, Long, Float>> sink;
 
-    /** Creates a job using the source and sink provided. */
-    public HourlyTipsExercise(
-            SourceFunction<TaxiFare> source, SinkFunction<Tuple3<Long, Long, Float>> sink) {
+  /**
+   * Creates a job using the source and sink provided.
+   */
+  public HourlyTipsExercise(
+          SourceFunction<TaxiFare> source, SinkFunction<Tuple3<Long, Long, Float>> sink) {
 
-        this.source = source;
-        this.sink = sink;
-    }
+    this.source = source;
+    this.sink = sink;
+  }
 
-    /**
-     * Main method.
-     *
-     * @throws Exception which occurs during job execution.
-     */
-    public static void main(String[] args) throws Exception {
+  /**
+   * Main method.
+   *
+   * @throws Exception which occurs during job execution.
+   */
+  public static void main(String[] args) throws Exception {
 
-        HourlyTipsExercise job =
-                new HourlyTipsExercise(new TaxiFareGenerator(), new PrintSinkFunction<>());
+    HourlyTipsExercise job =
+            new HourlyTipsExercise(new TaxiFareGenerator(), new PrintSinkFunction<>());
 
-        job.execute();
-    }
+    job.execute();
+  }
 
-    /**
-     * Create and execute the hourly tips pipeline.
-     *
-     * @return {JobExecutionResult}
-     * @throws Exception which occurs during job execution.
-     */
-    public JobExecutionResult execute() throws Exception {
+  /**
+   * Create and execute the hourly tips pipeline.
+   *
+   * @return {JobExecutionResult}
+   * @throws Exception which occurs during job execution.
+   */
+  public JobExecutionResult execute() throws Exception {
 
-        // set up streaming execution environment
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    // set up streaming execution environment
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // start the data generator
-        DataStream<TaxiFare> fares = env.addSource(source);
+    // start the data generator
+    DataStream<TaxiFare> fares = env.addSource(source);
 
-        // replace this with your solution
-        if (true) {
-            throw new MissingSolutionException();
-        }
+    fares
+            .assignTimestampsAndWatermarks(WatermarkStrategy
+                    .<TaxiFare>forBoundedOutOfOrderness(Duration.ofSeconds(20))
+                    .withTimestampAssigner((tf, timestamp) -> tf.getEventTimeMillis()))
 
-        // the results should be sent to the sink that was passed in
-        // (otherwise the tests won't work)
-        // you can end the pipeline with something like this:
+            .keyBy(fare -> fare.driverId)
+            .window(TumblingEventTimeWindows.of(Time.hours(1)))
+            .aggregate(new AggregateFunction<TaxiFare, Tuple3<Long, Long, Float>, Tuple3<Long, Long, Float>>() {
+              @Override
+              public Tuple3<Long, Long, Float> createAccumulator() {
+                return new Tuple3<>(null, null, 0f);
+              }
 
-        // DataStream<Tuple3<Long, Long, Float>> hourlyMax = ...
-        // hourlyMax.addSink(sink);
+              @Override
+              public Tuple3<Long, Long, Float> add(TaxiFare tf, Tuple3<Long, Long, Float> acc) {
+                return new Tuple3<>(tf.getEventTimeMillis(), tf.driverId, tf.tip + acc.f2);
+              }
 
-        // execute the pipeline and return the result
-        return env.execute("Hourly Tips");
-    }
+              @Override
+              public Tuple3<Long, Long, Float> getResult(Tuple3<Long, Long, Float> o) {
+                return new Tuple3<>(
+                        Instant.ofEpochMilli(o.f0)
+                                .plus(1, ChronoUnit.HOURS)
+                                .truncatedTo(ChronoUnit.HOURS)
+                                .toEpochMilli(),
+                        o.f1,
+                        o.f2
+                );
+              }
+
+              @Override
+              public Tuple3<Long, Long, Float> merge(Tuple3<Long, Long, Float> acc1, Tuple3<Long, Long, Float> acc2) {
+                return new Tuple3<>(
+                        acc1.f0 != null ? acc1.f0 : acc2.f0,
+                        acc1.f1 != null ? acc1.f1 : acc2.f1,
+                        acc1.f2 + acc2.f2
+                );
+              }
+            })
+
+            .keyBy(t3 -> t3.f0)
+            .map(new MapFunction<Tuple3<Long, Long, Float>, Tuple3<Long, Long, Float>>() {
+              @Override
+              public Tuple3<Long, Long, Float> map(Tuple3<Long, Long, Float> t) throws Exception {
+                System.out.println(t);
+                return t;
+              }
+            })
+
+            .keyBy(t3 -> t3.f0)
+            .window(TumblingEventTimeWindows.of(Time.hours(1)))
+            .aggregate(new AggregateFunction<Tuple3<Long, Long, Float>, Tuple3<Long, Long, Float>, Tuple3<Long, Long, Float>>() {
+              @Override
+              public Tuple3<Long, Long, Float> createAccumulator() {
+                return new Tuple3<>(null, null, 0f);
+              }
+
+              @Override
+              public Tuple3<Long, Long, Float> add(Tuple3<Long, Long, Float> event, Tuple3<Long, Long, Float> acc) {
+                if (acc.f0 == null) return event;
+                return event.f2 > acc.f2 ? event : acc;
+              }
+
+              @Override
+              public Tuple3<Long, Long, Float> getResult(Tuple3<Long, Long, Float> o) {
+                return o;
+              }
+
+              @Override
+              public Tuple3<Long, Long, Float> merge(Tuple3<Long, Long, Float> acc1, Tuple3<Long, Long, Float> acc2) {
+                return add(acc1, acc2);
+              }
+            })
+
+            .map(new MapFunction<Tuple3<Long, Long, Float>, Tuple3<Long, Long, Float>>() {
+              @Override
+              public Tuple3<Long, Long, Float> map(Tuple3<Long, Long, Float> t) throws Exception {
+                System.out.println(t);
+                return t;
+              }
+            })
+
+            .addSink(sink);
+
+
+    // the results should be sent to the sink that was passed in
+    // (otherwise the tests won't work)
+    // you can end the pipeline with something like this:
+
+    // DataStream<Tuple3<Long, Long, Float>> hourlyMax = ...
+    // hourlyMax.addSink(sink);
+
+    // execute the pipeline and return the result
+    return env.execute("Hourly Tips");
+  }
 }
